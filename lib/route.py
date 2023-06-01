f9a4b8550c30ab628ef85afb86369501793eeef5
pokr <mistrpokr@outlook.com>
2023-06-01 12:07:51 -0700

    make BnB usable in main app; move Node and friends to core.py

--- lib/route.py
+++ lib/route.py
@@ -4,135 +4,7 @@ from __future__ import annotations
 import heapq
 from itertools import combinations, product
 from .core import *
-
-
-class AccessPoint:
-    def __init__(self, coord: tuple[int, int], parent: Node) -> None:
-        self.coord = coord
-        self.parent = parent  # Pointer to parent node
-        self.dv = dict()  # Initialize distance vector
-
-    def add_path(
-        self, destination: AccessPoint, distance: int, path: list[tuple[int, int]]
-    ) -> None:
-        """
-        Add a path to the distance vector. The path goes from this AP
-        to another AP with a tuple containing the path's distance and
-        grid-by-grid path to take
-        """
-        self.dv[destination] = (distance, path)
-        destination.dv[self] = (distance, path[::-1])
-
-    def get_nearest_ap(self) -> tuple[int, list[tuple[int, int]]]:
-        """
-        Return the nearest AP stored in the current AP's distance vector
-        """
-        return min(self.dv.items(), key=lambda item: item[1][0])
-
-
-class Node:
-    """
-    A Node describes an item in the warehouse. Aside from an ID, it has
-    a coordinate and up to 4 APs (AccessPoint, defined in the AccessPoint class).
-
-    Each Node instance is intended to be initialized with a map. The
-    map binding should not be changed once created, as the underlying
-    AccessPoints depend on the map.
-
-    After creation of Nodes, the distance vectors in their APs are empty
-    and do not store any distance information. generate_cost_graph()
-    must be called to calculate and create that information, see its
-    documentation for usage.
-    """
-
-    def __init__(self, id: int, coord: tuple[int, int], map) -> None:
-        self.id, self.coord = id, coord
-
-        # The product's neighbors; initialized with empty elements and will be updated later
-        self._ap = {"n": None, "e": None, "s": None, "w": None}
-        # Bind reference to the map from which this product instance was created
-        self._map = map
-
-        # Detect valid neighbors (access points)
-        aps = get_aps(self._map, self.coord)
-        # Assign each neighbor to a direction (n/s/e/w)
-        for direction, ap in aps:
-            match direction:
-                case (1, 0):
-                    self._ap["n"] = AccessPoint(coord=ap, parent=self)
-                case (0, 1):
-                    self._ap["e"] = AccessPoint(coord=ap, parent=self)
-                case (-1, 0):
-                    self._ap["s"] = AccessPoint(coord=ap, parent=self)
-                case (0, -1):
-                    self._ap["w"] = AccessPoint(coord=ap, parent=self)
-
-    @property
-    def x(self) -> int:
-        return self.coord[0]
-
-    @property
-    def y(self) -> int:
-        return self.coord[1]
-
-    @property
-    def n(self) -> AccessPoint:
-        return self._ap["n"]
-
-    @property
-    def e(self) -> AccessPoint:
-        return self._ap["e"]
-
-    @property
-    def s(self) -> AccessPoint:
-        return self._ap["s"]
-
-    @property
-    def w(self) -> AccessPoint:
-        return self._ap["w"]
-
-    @property
-    def aps(self):
-        return [v for v in self._ap.values() if v]
-
-    @property
-    def aps_all(self) -> list[AccessPoint]:
-        """
-        Return a list of neighbors containing empty directions,
-        For example, the first element would be None if the north AP doesn't exist
-        """
-        return list(self._ap.values())
-
-
-class SingleNode(Node):
-    """
-    Single Access Nodes that are on both "ends" of the nodes list, i.e.
-    the starting or ending nodes. SingleNode have a item ID of 0 and a north
-    AP that has the same coordinate as the SingleNode themselves, so that they
-    can be treated like standard Nodes in algorithms.
-    """
-
-    def __init__(self, coord, map, access_self=True):
-        super().__init__(-1, coord, map)  # Initialize APs like a normal Node
-
-        if access_self:
-            # If the node's grid itself can be accessed,
-            # i.e. Start/End nodes where no AP is necessary
-            self._ap["n"] = AccessPoint(coord=self.coord, parent=self)
-            # Remove all APs except the north one, and set it to have the
-            # same coordinate as the node
-            for direction in ["e", "s", "w"]:
-                self._ap[direction] = None
-        else:
-            # Keep the first available AP and drop others
-            # Has a priority: N-E-S-W
-            drop = True
-            for direction in ["n", "e", "s", "w"]:
-                if not drop:
-                    self._ap[direction] = None
-                if self._ap[direction]:
-                    drop = False  # Drop APs on other directions
-
+from .bnb import *
 
 def prod_to_node(prod: Prod):
     return Node(prod.id, (prod.x, prod.y), prod._map)
@@ -393,7 +265,9 @@ def find_route(
     generate_cost_graph(nodes, start_node=start_node, end_node=end_node)
 
     if algorithm == "b":  # branch and bound
-        print("WIP")
+        total_cost, path = branch_and_bound(
+            item_nodes=item_nodes, start_node=start_node, end_node=end_node
+        )
     elif algorithm == "g":  # greedy
         total_cost, path = greedy(nodes, start_ap, end_ap, init_ap=start_ap)
     elif algorithm == "n":
